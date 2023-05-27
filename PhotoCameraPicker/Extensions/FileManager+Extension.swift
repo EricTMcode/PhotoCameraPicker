3767283edb184d52c98ff760bce38a21670ce514
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-27 15:37:45 +0200

    - add the saveDocument and readDocument methods into FileManager

--- PhotoCameraPicker/Extensions/FileManager+Extension.swift
+++ PhotoCameraPicker/Extensions/FileManager+Extension.swift
@@ -19,6 +19,20 @@ extension FileManager {
     }
     
     func saveDocument(contents: String) throws {
-        
+        let url = Self.docDirURL.appendingPathComponent(fileName)
+        do {
+            try contents.write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            throw MyImageError.saveError
+        }
+    }
+    
+    func readDocument() throws -> Data {
+        let url = Self.docDirURL.appendingPathComponent(fileName)
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            throw MyImageError.readError
+        }
     }
 }
