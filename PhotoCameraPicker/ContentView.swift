b8a6be4fd8655bd47dc972a4d892a454b39277f0
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-27 14:30:41 +0200

    Initial Commit

---
+++ PhotoCameraPicker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PhotoCameraPicker
+//
+//  Created by Eric on 27/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoCameraPicker/PhotoCameraPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCameraPickerApp.swift
+//  PhotoCameraPicker
+//
+//  Created by Eric on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCameraPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
